e9a17490d126499c5933ecbaf33d5079c8c1f04f
Talita Beneves <talitabeneves15@gmail.com>
2023-04-08 16:29:26 -0300

    feat: criando model + db

---
+++ Progress.Finance.API/Progress.Finance.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Model;
+
+namespace API.Data
+{
+    public class DataContext : DbContext //NOME PADÃO
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Metas> progress { get; set; }
+
+        public DbSet<Items> items { get; set; }
+    }
+}
---
+++ Progress.Finance.API/Progress.Finance.API/Model/Metas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace API.Model
+{
+    public class Metas
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NomeMeta { get; set; }
+        public int ValorInicial { get; set; }
+        public int ValorMeta { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAlvo { get; set; }
+        public int Porcentagem { get; set; }
+        public Status Status { get; set; }
+
+        public virtual ICollection<Items> Items { get; set; }
+    }
+
+    public class Items
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ValorDepositado { get; set; }
+        public DateTime DataDeposito { get; set; }
+
+        // Referência para o objeto pai
+        public int IdMeta { get; set; }
+        public int ProgressFinanceModelId { get; set; }
+
+        [JsonIgnore]
+        public virtual Metas ProgressFinanceModel { get; set; }
+    }
+}
